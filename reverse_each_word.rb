4be0eb72a0bf8edded985528d5933efb9a22c9ae
Min Hee  Park <minheekangg@gmail.com>
2018-09-29 01:01:29 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,12 @@ def reverse_each_word(sentence1)
   end
   return new_new.join(" ")
 end
+
+def reverse_each_word(sentence1)
+  new_sentence = sentence1.split(" ")
+  new_new = []
+  new_sentence.collect do |word|
+    new_new << word.reverse
+  end
+  return new_new.join(" ")
+end
